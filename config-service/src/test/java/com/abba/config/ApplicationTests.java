d2ff9c1fd8f287d99428a24295f16ef71613c756
Lucky.Yang <wanpinwei@outlook.com>
2020-03-04 20:30:00 +0800

    添加测试用例

--- config-service/src/test/java/com/abba/config/ApplicationTests.java
+++ config-service/src/test/java/com/abba/config/ApplicationTests.java
@@ -2,14 +2,12 @@ package com.abba.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
@@ -21,21 +19,14 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 public class ApplicationTests {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Test
     public void configurationAvailable() {
         @SuppressWarnings("rawtypes")
-        ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
-                "/app/cloud", Map.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-    }
-
-    @Test
-    public void envPostAvailable() {
-        MultiValueMap<String, String> form = new LinkedMultiValueMap();
-        @SuppressWarnings("rawtypes")
-        ResponseEntity<Map> entity = new TestRestTemplate().postForEntity(
-                "/admin/env", form, Map.class);
+        ResponseEntity<Map> entity = testRestTemplate.withBasicAuth(
+                "user", "abba.123").getForEntity("/app/cloud", Map.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
     }
 
